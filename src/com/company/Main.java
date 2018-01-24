9ba4bfb0369e2c95ad081e26f68bace4e5493a72
JustinYates <Justin.yates24601@gmail.com>
2018-01-24 14:18:44 -0500

    Creating project

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Math.pow(3,5) = " + Math.pow(3,5));
+        System.out.println(10.0 / 7);
+
+        System.out.println("5 * Math.pow(2, 5) - 4 = " + (5 *Math.pow(2, 5) - 4));
+
+        System.out.println(10.3 % 5);
+    }
+}
